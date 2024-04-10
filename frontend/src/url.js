d4d1b02e0b2851cb4ea87ae5b6f8c43da13ffc0d
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2024-04-11 02:27:35 +0530

    Update url.js

--- frontend/src/url.js
+++ frontend/src/url.js
@@ -1,2 +1,5 @@
-export const URL="https://mern-blog-app-b9gr.onrender.com"
-export const IF="https://mern-blog-app-b9gr.onrender.com/images/"
+// export const URL="https://mern-blog-app-b9gr.onrender.com"
+// export const IF="https://mern-blog-app-b9gr.onrender.com/images/"
+
+export const URL=import.meta.env.VITE_URL
+export const IF=import.meta.env.VITE_IF
