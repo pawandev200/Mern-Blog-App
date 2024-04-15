acc0153059b46ebf4dbfc2f5810522e37e9d91c4
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2024-04-15 15:32:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"*"}))
+app.use(cors({origin:"*",credentials:false}))
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
