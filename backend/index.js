2ebcaead67910560c828c90bd42cf784ced65394
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2024-04-15 15:06:17 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({"Access-Control-Allow-Origin": "*"}))
+app.use(cors({credentials: true, origin: 'https://mern-blog-app-frontend-2.onrender.com/'}));
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
