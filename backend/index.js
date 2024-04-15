e076a3e4c8ea692966324a45eba145f693c7b3f3
Pawandev Kumar <158770456+pawandev200@users.noreply.github.com>
2024-04-15 15:35:53 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -28,7 +28,7 @@ const connectDB=async()=>{
 dotenv.config()
 app.use(express.json())
 app.use("/images",express.static(path.join(__dirname,"/images")))
-app.use(cors({origin:"*",credentials:false}))
+app.use(cors({credentials: true, origin: 'https://mern-blog-app-e1zu.vercel.app'}));
 app.use(cookieParser())
 app.use("/api/auth",authRoute)
 app.use("/api/users",userRoute)
